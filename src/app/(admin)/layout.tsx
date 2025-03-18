0c1059723fa144d08112385dd3925f2ff660057a
Diego Rondón <furrtr4532@gmail.com>
2025-03-18 16:36:12 -0500

    feat: eliminar la barra de navegación superior y reubicar el componente de búsqueda

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,26 +1,17 @@
 import AdminLayout from "@/components/layout/admin-layout";
 import { Header } from "@/components/layout/header";
 import { Main } from "@/components/layout/main";
-import { TopNav } from "@/components/layout/top-nav";
 import { ProfileDropdown } from "@/components/profile-dropdown";
 import { Search } from "@/components/search";
 import { ThemeSwitch } from "@/components/theme-switch";
-
-const topNav = [
-  { title: "Overview", href: "/admin/dashboard/overview", isActive: true },
-  { title: "Customers", href: "/admin/dashboard/customers", isActive: false, disabled: true },
-  { title: "Products", href: "/admin/dashboard/products", isActive: false, disabled: true },
-  { title: "Settings", href: "/admin/dashboard/settings", isActive: false, disabled: true },
-];
 
 export default function AdminLayoutWrapper({ children }: { children: React.ReactNode }) {
   return (
     <AdminLayout>
       {/* ===== Top Heading ===== */}
       <Header>
-        <TopNav links={topNav} />
+        <Search />
         <div className="ml-auto flex items-center space-x-4">
-          <Search />
           <ThemeSwitch />
           <ProfileDropdown />
         </div>
