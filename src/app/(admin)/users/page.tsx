10707f9f9a6a22abf6fb259e58a40012f961a067
Diego Rondón <furrtr4532@gmail.com>
2025-03-18 17:29:41 -0500

    feat: agregar manejo de errores en la página de usuarios con componente de error general

--- src/app/(admin)/users/page.tsx
+++ src/app/(admin)/users/page.tsx
@@ -1,17 +1,37 @@
 "use client";
 
 import { HeaderPage } from "@/components/common/HeaderPage";
+import ErrorGeneral from "@/components/errors/general-error";
 import { UsersTable } from "./_components/table/UserTable";
 import { useUsers } from "./_hooks/use-users";
 
 export default function UsersPage() {
   const { data, isLoading } = useUsers();
 
-  console.log(data, isLoading);
+  if (isLoading) {
+    return (
+      <div>
+        <HeaderPage title="Usuarios" description="Usuarios registrados en el sistema." />
+        <div className="flex flex-col items-end justify-center gap-4">{/* Contenido mientras carga */}</div>
+      </div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div>
+        <HeaderPage title="Usuarios" description="Usuarios registrados en el sistema." />
+        <ErrorGeneral />
+      </div>
+    );
+  }
+
   return (
     <div>
       <HeaderPage title="Usuarios" description="Usuarios registrados en el sistema." />
-      <UsersTable data={data ?? []} />
+      <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <UsersTable data={data} />
+      </div>
     </div>
   );
 }
