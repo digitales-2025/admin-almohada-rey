1713b977024b6949af9c48fbc74a768c09fc4629
DiegoRondonR <furrtr4532@gmail.com>
2025-03-26 15:55:10 -0500

    feat: añadir archivo de ejemplo .env y actualizar configuración de imágenes en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN ?? ""],
+  },
 };
 
 export default nextConfig;
