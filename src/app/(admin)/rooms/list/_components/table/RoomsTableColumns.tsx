214703954f462109fb2427cea6c22f03c83c2365
DiegoRondonR <furrtr4532@gmail.com>
2025-03-28 17:02:24 -0500

    feat: agregar diálogos para eliminar y reactivar habitaciones en la tabla de habitaciones

--- src/app/(admin)/rooms/list/_components/table/RoomsTableColumns.tsx
+++ src/app/(admin)/rooms/list/_components/table/RoomsTableColumns.tsx
@@ -18,6 +18,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Room, RoomStatus } from "../../_types/room";
 import { getRoomTypeKey, RoomStatusLabels, RoomTypeLabels } from "../../_utils/rooms.utils";
+import { DeleteRoomsDialog } from "../state-management/DeleteRoomsDialog";
+import { ReactivateRoomsDialog } from "../state-management/ReactivateRoomsDialog";
 import { UpdateRoomSheet } from "../update/UpdateRoomsSheet";
 import { RoomNumberCell } from "./RoomNumberCell";
 import { RoomImageCell } from "./view-image/RoomImageViewer";
@@ -177,6 +179,29 @@ export const roomsColumns = (isSuperAdmin: boolean): ColumnDef<Room>[] => [
           <div>
             {showEditDialog && (
               <UpdateRoomSheet open={showEditDialog} onOpenChange={setShowEditDialog} room={row?.original} />
+            )}
+
+            {showDeleteDialog && (
+              <DeleteRoomsDialog
+                open={showDeleteDialog}
+                onOpenChange={setShowDeleteDialog}
+                rooms={[row?.original]}
+                showTrigger={false}
+                onSuccess={() => {
+                  row.toggleSelected(false);
+                }}
+              />
+            )}
+            {showReactivateDialog && (
+              <ReactivateRoomsDialog
+                open={showReactivateDialog}
+                onOpenChange={setShowReactivateDialog}
+                rooms={[row?.original]}
+                showTrigger={false}
+                onSuccess={() => {
+                  row.toggleSelected(false);
+                }}
+              />
             )}
           </div>
           <DropdownMenu>
