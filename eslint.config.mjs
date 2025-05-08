e79191b33fb333b31a88c7f7bf7088c824c5e42b
Fernando Araoz <fernando@araozu.dev>
2025-05-08 17:12:21 -0500

    ci: improve disk usage pt. 4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -57,13 +57,10 @@ export default [
       parser: tsParser,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-				"error",
-				{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
-			],
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off",
-      "no-use-before-define": [0],
+      "no-use-before-define": "off",
       "@typescript-eslint/no-use-before-define": [1],
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-var-requires": "off",
